a88be1047b2010eccb17cfa450079525145afa37
smyzin <smyzin.work@gmail.com>
2016-08-10 18:31:59 +0300

    Add
    Add files to repo

---
+++ basic_algorithm_scripting/factorialize_a_number.js
@@ -0,0 +1,14 @@
+/*
+Return the factorial of the provided integer.
+If the integer is represented with the letter n, a factorial is the product of all positive integers less than or equal to n.
+*/
+
+function factorialize(num) {
+  if(num === 0 || num == 1){
+    return 1;
+  }else{
+    return num * factorialize(num - 1);
+  }
+}
+
+factorialize(5);
